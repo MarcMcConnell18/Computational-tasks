62f8e88b1f6312fdc5ce064f83d2424b0a6ed0d0
Yujie Jia <149315814+MarcMcConnell18@users.noreply.github.com>
2023-11-30 17:54:14 +0000

    Update demo_audio_restoration.py

--- demo_audio_restoration.py
+++ demo_audio_restoration.py
@@ -127,13 +127,13 @@ print("The MSE for cubic spline is:", error2)
 wavfile.write('output_medianfilter.wav', sample_rate1, restored1)
 wavfile.write('output_cubicspline.wav', sample_rate1, restored2)
 
-# play the audio
-# sd.play(wavsignal, sample_rate1)
-# sd.wait()
-# sd.play(restored1, sample_rate1)
-# sd.wait()
-# sd.play(restored2, sample_rate1)
-# sd.wait()
+play the audio
+sd.play(wavsignal, sample_rate1)
+sd.wait()
+sd.play(restored1, sample_rate1)
+sd.wait()
+sd.play(restored2, sample_rate1)
+sd.wait()
 
 
 # unit test
